9e76cd86d84893e031bed6c8bdb80b09bb18e986
andreiyugurau <andreiyugurau@gmail.com>
2017-10-03 13:41:56 -0400

    Added database

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://sishun:sishun@ds161164.mlab.com:61164/schoolnotesmagazine',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
